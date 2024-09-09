e84e28f081bfda131dba432bd417c1104130ec28
Dinidu Sri Nimesha <131584114+dinidusrinimesha@users.noreply.github.com>
2024-09-10 01:11:38 +0530

    2024-09-09

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
 import React from "react";
-import Header from "./components/Header";
-import Navbar from "./components/Navbar";
 import StudentBio from "./components/StudentBio";
-
-import "./App.css";
 
 function App() {
   return (
     <body>
       <div className="App">
-        <Header />
-        <Navbar />
         <StudentBio />
       </div>
     </body> 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography, Button, Container } from '@mui/material';
+import styles from './StudentBio.module.css';
 
 function Header() {
   return (
-    <AppBar position="static" sx={{ backgroundColor: '#CDC1D6' }}>
-      <Container>
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1, color: 'black' }}>
-            ABC School
-          </Typography>
-          <Button color="inherit" sx={{color: 'black' }}>Contact Us: 071123123</Button>
-        </Toolbar>
-      </Container>
-    </AppBar>
+    <header className={styles.header}>
+      <div className={styles.schoolInfo}>
+        <img src="https://cdn.builder.io/api/v1/image/assets/TEMP/c77009e8035674cdb59687b4b8c4ec1149fb57d2e347bffb7ef35c6e64ee6260?placeholderIfAbsent=true&apiKey=540abe8e50444d9db0d29b519fdb50e8" alt="ABC School Logo" className={styles.schoolLogo} />
+        <h1 className={styles.schoolName}>ABC School</h1>
+      </div>
+      <div className={styles.contactInfo}>
+        <button className={styles.contactButton}>Contact Us</button>
+        <div className={styles.phoneWrapper}>
+          <img src="https://cdn.builder.io/api/v1/image/assets/TEMP/e7b4cfc33ef616b0ca9d6c7ee6905e08982aacd6bcd6f616e9b40ecb51961e30?placeholderIfAbsent=true&apiKey=540abe8e50444d9db0d29b519fdb50e8" alt="" className={styles.phoneIcon} />
+          <span className={styles.phoneNumber}>(011) 2345 567</span>
+        </div>
+      </div>
+    </header>
   );
 }
 
